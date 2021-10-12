cff9f832d154f0c081fa43c9420dee286ca48c95
Sawsj25 <sawsj25@gmail.com>
2021-10-12 11:31:44 +0330

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+include 'functions.php';
+
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<h1>Welcome to the This page!</h1>
+</div>
+
+<?=template_footer()?>
